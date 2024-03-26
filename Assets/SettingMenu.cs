8fe38e7153bb879d3b873aff0e564543fece10e2
Haroon Sohail <haroonsohail323@gmail.com>
2024-03-27 03:18:28 +0500

    OptionMenu Implemented

--- Assets/SettingMenu.cs
+++ Assets/SettingMenu.cs
@@ -1,23 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SettingMenu : MonoBehaviour
+public class OptionMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider volumeSlider;
+    public Button applyButton;
+    public GameObject Optionmenu;
+    public GameObject mainMenu;
+
     void Start()
     {
-        
+        // Load saved volume (if any)
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+            SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            // Set default volume if it's not yet set
+            volumeSlider.value = 1f;
+            SetVolume(1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume(float volume)
     {
-        
+        // Set volume
+        AudioListener.volume = volume;
+
+        // Save volume
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 
-    public void SetVolume()
+    public void ApplyChanges()
     {
-        
+        // Apply any other changes here if necessary
+        Debug.Log("Changes applied!");
+        Optionmenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
